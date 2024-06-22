7028879989923418169e9cc30350164473165594
DESKTOP-3ON4F1P\Youssef Abdellah <youssef.joo350@gmail.com>
2024-06-23 01:51:24 +0300

    Update

--- src/test/java/comp_loginpage/LoginPageComp.java
+++ src/test/java/comp_loginpage/LoginPageComp.java
@@ -37,7 +37,7 @@ public class LoginPageComp extends LoginPageWebElement{
         return new Object[][]{
                 {"standard_user","secret_sauce"},
                 {"locked_out_user","secret_sauce"},
-                {"problem_user","secret_sauce"},
+//                {"problem_user","secret_sauce"},
 //                {"performance_glitch_user","secret_sauce"},
 //                {"error_user","secret_sauce"},
 //                {"visual_user","secret_sauce"}
